5e07a5b322778bebaf8f46637337096377f7d4c8
Ashutosh Kumar <ashutosh981209@gmail.com>
2026-07-08 22:28:59 +0530

    Add files via upload

---
+++ 03 _useroperators.sql
@@ -0,0 +1,7 @@
+Use startersql;
+-- SELECT * FROM startersql.users;
+-- select *from users where date_of_birth between '1990-09-09' and '1999-09-09';
+-- select *from users where gender in ('Male','Female')
+-- select *from users where gender='Female' and salary>'70000';
+ --  select *from users name order by name asc
+  
